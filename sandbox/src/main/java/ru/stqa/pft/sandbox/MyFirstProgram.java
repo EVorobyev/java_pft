1e3978f4ccd425b317843113c7bf0e3d5d33b57e
EVorobyev <e.vorobiev@list.ru>
2017-07-15 01:01:22 +0300

    Расстояние между точками (без метода).

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,10 +12,25 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point();
+    p1.x = 3;
+    p1.y = 6;
+
+    Point p2 = new Point();
+    p2.x = 5;
+    p2.y = 9;
+    System.out.println ("Расстояние на координатной оси между точками c координатами " + p1.x + ";"+ p1.y + " и " + p2.x + ";"+ p2.y + " = " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2){
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by EOnegin on 14.07.2017.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+}
