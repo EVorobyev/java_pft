dc3ba98913fced9998ad4cdb3031047e35219cde
EVorobyev <e.vorobiev@list.ru>
2017-08-07 23:58:40 +0300

    Реализована сортировка в CreationTests (не учитываем Id)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -12,7 +12,7 @@ public class ContactData {
   private final String email;
 
   public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String mobile, String email) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.middlename = middlename;
     this.lastname = lastname;
@@ -83,7 +83,6 @@ public class ContactData {
             ", lastname='" + lastname + '\'' +
             '}';
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -91,17 +90,14 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != that.id) return false;
     if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
     return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
     return result;
   }
-
 }
