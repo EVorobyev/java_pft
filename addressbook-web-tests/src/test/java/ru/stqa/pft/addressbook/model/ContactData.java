322384b9bdf8c343072771a093d4d2f1e463ff93
EVorobyev <e.vorobiev@list.ru>
2017-08-14 16:14:55 +0300

    Реализ.метод обратных проверок(в ContactPhoneTests)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -11,7 +11,17 @@ public class ContactData {
   private String mobile;
   private String homephone;
   private String workphone;
+  private String allPhones;
   private String email;
+
+  public String getAllPhones() {
+    return allPhones;
+  }
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
 
   public int getId() {
     return id;
