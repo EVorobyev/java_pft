aaa58bfbd82e5468142544e8dd72a0f54e72e655
EVorobyev <e.vorobiev@list.ru>
2017-08-14 22:55:04 +0300

    Реализован тест testContactPostAddress

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPostAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPostAddressTests.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by EOnegin on 14.08.2017.
@@ -17,4 +22,12 @@ public class ContactPostAddressTests extends TestBase {
     }
   }
 
+  @Test
+  public void testContactPostAddress() {
+    app.goTo().home();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+  }
 }
