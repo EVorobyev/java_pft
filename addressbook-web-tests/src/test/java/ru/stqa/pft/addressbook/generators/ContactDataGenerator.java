72f9f8f75bcd0978d5e76e7da44e2ad0c8b731f3
EVorobyev <e.vorobiev@list.ru>
2017-08-17 01:59:16 +0300

    Обраб.опции команд.строки при помощи jcommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -14,15 +17,30 @@ import java.util.List;
  */
 public class ContactDataGenerator {
 
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
 
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
       writer.write(String.format("%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getMiddlename(), contact.getLastname(), contact.getNickname(), contact.getTitle(), contact.getCompany()));
@@ -30,7 +48,7 @@ public class ContactDataGenerator {
     writer.close();
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("testfirstname %s", i)).withMiddlename(String.format("testmiddlename %s", i)).withLastname(String.format("testlastname %s", i))
