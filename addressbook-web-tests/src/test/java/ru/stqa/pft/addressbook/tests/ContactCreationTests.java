da58fa2d3498557899c2d477b4b1319f8a5a03ee
EVorobyev <e.vorobiev@list.ru>
2017-08-17 03:41:39 +0300

    Реализована параметризация тест.данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,38 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("testfirstname 1").withMiddlename("testmiddlename 1").withLastname("testlastname 1")
+            .withNickname("testnickname 1").withTitle("testtitle 1").withCompany("testcompany 1")});
+    list.add(new Object[] {new ContactData().withFirstname("testfirstname 2").withMiddlename("testmiddlename 2").withLastname("testlastname 2")
+            .withNickname("testnickname 2").withTitle("testtitle 2").withCompany("testcompany 2")});
+    list.add(new Object[] {new ContactData().withFirstname("testfirstname 3").withMiddlename("testmiddlename 3").withLastname("testlastname 3")
+            .withNickname("testnickname 3").withTitle("testtitle 3").withCompany("testcompany 3")});
+    return list.iterator();
+
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().home();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("testfirstname2").withMiddlename("testmiddlename1").withLastname("testlastname1")
-            .withNickname("testnickname1").withTitle("testtitle1").withCompany("testcompany1").withMobile("testmobile1").withEmail("testemail1").withPhoto(photo);
     app.contact().create(contact);
     app.goTo().home();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
