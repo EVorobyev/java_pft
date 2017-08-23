3c3779ac3f5136af7802c890cb6d5009a3f4d5be
EVorobyev <e.vorobiev@list.ru>
2017-08-23 23:13:09 +0300

    DbConnection tests for groups and contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -12,10 +14,10 @@ import java.sql.*;
 public class DbConnectionTest {
 
   @Test
-  public void testDbConnction () {
+  public void testDbConnection() {
     Connection conn = null;
     try {
-      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
       Groups groups = new Groups();
@@ -35,5 +37,31 @@ public class DbConnectionTest {
       System.out.println("SQLState: " + ex.getSQLState());
       System.out.println("VendorError: " + ex.getErrorCode());
     }
+
+    Connection connc = null;
+    try {
+      connc = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
+      Statement st = connc.createStatement();
+      ResultSet rsc = st.executeQuery("select id, firstname, middlename, lastname, nickname, title, company from addressbook");
+      Contacts contacts = new Contacts();
+      while (rsc.next()) {
+        contacts.add(new ContactData().withId(rsc.getInt("id")).withFirstname(rsc.getString("firstname"))
+                .withMiddlename(rsc.getString("middlename")).withLastname(rsc.getString("lastname")).withNickname(rsc.getString("nickname"))
+                .withTitle(rsc.getString("title")).withCompany(rsc.getString("company")));
+      }
+      rsc.close();
+      st.close();
+      connc.close();
+
+      System.out.println(contacts);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+
+    }
   }
 }
+
