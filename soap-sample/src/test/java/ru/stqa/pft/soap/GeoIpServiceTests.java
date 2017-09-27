e2a299dc772ae1a2d78cc51fead19adafe1c2a83
EVorobyev <e.vorobiev@list.ru>
2017-09-27 16:35:37 +0300

    SOAP: простой пример

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by EOnegin on 27.09.2017.
+ */
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("188.68.184.117");
+    assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+
+  @Test
+  public void testInvalidIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("188.68.184.xxx");
+    assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+
+}
