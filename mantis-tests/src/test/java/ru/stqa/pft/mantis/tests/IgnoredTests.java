b06d292e7bf3dc856a01ffb9b30040085e3872ba
EVorobyev <e.vorobiev@list.ru>
2017-09-30 19:21:55 +0300

    Реализована интегр. тестов с БТ MantisBT

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/IgnoredTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
+/**
+ * Created by EOnegin on 30.09.2017.
+ */
+public class IgnoredTests extends TestBase{
+
+  private int id=1;
+
+
+  @Test
+  public void testIgnored() throws RemoteException, ServiceException, MalformedURLException {
+    skipIfNotFixed(id);
+    System.out.println("Test is enabled");
+  }
+
+}
